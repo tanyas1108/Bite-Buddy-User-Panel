3d8ddfcbe944bc8d18b3d06260e1487765be205b
Tanya Sharma <122716588+tanyas1108@users.noreply.github.com>
2025-02-06 11:21:58 +0530

    Add files via upload

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import React, { useContext, useState } from "react";
+import { StoreContext } from "../../context/StoreContext";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = ({ setShowLogin }) => {
+  const { getTotalQuantity, token, setToken } = useContext(StoreContext);
+  const totalQuantity = getTotalQuantity();
+
+  const [menu, setMenu] = useState("home");
+
+  const navigate = useNavigate(); 
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  }
+
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search_icon" />
+        <div className="navbar-basket-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="basket_icon" />
+          </Link>
+          <div className={totalQuantity === 0 ? "dotHidden" : "dot"}>
+            <p>{totalQuantity}</p>
+          </div>
+        </div>
+        {!token ? <button onClick={() => setShowLogin(true)}>Sign in</button> 
+        : <div className="navbar-profile">
+          <img src={assets.profile_icon} alt="" />
+          <ul className="nav-profile-dropdown">
+            <li onClick={() => navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <hr />
+            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+          </ul>
+        </div>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
